917e3a0e17411b4783dfd574cac722662fed7a85
me-zoen <zvehonsky000@student.rcschools.net>
2025-10-21 08:35:49 -0500

    Update the_gay.js

--- js/the_gay.js
+++ js/the_gay.js
@@ -2,3 +2,7 @@ let score = 0;
 
 const the_gay = document.getElementById("the_gay");
 const score_label = document.getElementById("score_label");
+
+the_gay.addEventListener('click', function() {
+  score += 1;
+});
