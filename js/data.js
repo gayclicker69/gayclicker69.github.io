63857eaf4ded1ac7397dc74cbdb25b021025ba41
gayclicker69 <gayclicker69@gmail.com>
2025-10-21 21:45:12 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,15 @@
-window.data = {
-  let score: 0;
-  let click_power: 1;
-	function unpack() {
-  }
-}
+// data.js  
+window.data = {  
+    score: 0,  
+    click_power: 1,  
+    unpack() {  
+        // Assign to global scope  
+        window.score = this.score;  
+        window.click_power = this.click_power;  
+    },  
+    pack() {  
+        // Update object from globals  
+        this.score = window.score;  
+        this.click_power = window.click_power;  
+    }  
+};
