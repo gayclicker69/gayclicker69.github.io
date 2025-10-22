55a9122b684e1dc1ae42bf76568121d7877b319a
gayclicker69 <gayclicker69@gmail.com>
2025-10-21 21:49:37 -0500

    Update the_gay.js

--- js/the_gay.js
+++ js/the_gay.js
@@ -1,12 +1,9 @@
-const data = window.data;
-data.unpack();
-
 const the_gay = document.getElementById("the_gay");
 const score_label = document.getElementById("score_label");
 
 the_gay.addEventListener('click', function() {
+  window.data.unpack();
   score += click_power;
   score_label.textContent = `gayness: ${score}`;
+  window.data.pack();
 });
-
-data.pack();
