058e208aaf1bb6b4cc43df7878c9761a52b9f11e
me-zoen <zvehonsky000@student.rcschools.net>
2025-10-23 08:41:25 -0500

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,14 @@
 window.data.unpack();
 
 const buy_men = document.getElementById("buy_men");
+const buy_men_label = documen.getElementById("buy_men_label");
 
-buy_men.addeventlistener(
+buy_men.addEventListener('click', function() {
+  if (score >= men.cost) {
+    score -= men.cost;
+    men.num += 1;
+    men.cost = math.floor(men.base_cost * math.pow(1.15, men.num));
+  }
+  
+  window.data.pack();
+});
