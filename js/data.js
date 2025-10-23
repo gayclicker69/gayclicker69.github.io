be75b0bfd905c511d9f448a9f257ad4b1eedcceb
me-zoen <zvehonsky000@student.rcschools.net>
2025-10-23 09:43:07 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -5,15 +5,17 @@ window.data = {
     men : {
     cost : 100,
     men_num : 0
-    }
+    },
     unpack() {  
-        // Assign to global scope  
+        // Assign to global scope
         window.score = this.score;  
-        window.click_power = this.click_power;  
+        window.click_power = this.click_power;
+        window.men = this.men;
     },  
     pack() {  
         // Update object from globals  
         this.score = window.score;  
-        this.click_power = window.click_power;  
+        this.click_power = window.click_power;
+        this.men = window.men;
     }  
 };
